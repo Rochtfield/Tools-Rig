ec046af1e0b6855695d4e66bb73f3ec10034302a
Rochtfield <160755865+Rochtfield@users.noreply.github.com>
2026-02-11 23:38:43 +0100

    ADD-Arm-FIX-Bug

--- BP_Skeleton.py
+++ BP_Skeleton.py
@@ -11,13 +11,6 @@ def Create_BlueprintSkeleton():
     cmds.setAttr(f"{pelvis_sphere}.rotateAxis", -90, 0, 90, type='double3')
     grpPelvis=cmds.group(empty=True, name='BP_Skeleton')
 
-    Color = cmds.shadingNode('lambert', asShader=True, name="Color_Shader")
-    cmds.setAttr(f"{Color}.color", 1.0 , 1.0, 0, type='double3')
-    sg = cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name="Blue_ShaderSG")
-    cmds.connectAttr(f"{Color}.outColor", f"{sg}.surfaceShader")
-    cmds.sets(pelvis_sphere, edit=True, forceElement=sg)
-    cmds.select(clear=True)
-
     #Spine
     spine_spheres = []
     spines_grp = []
@@ -38,14 +31,7 @@ def Create_BlueprintSkeleton():
     cmds.pointConstraint(pelvis_sphere, spine_spheres[1], spines_grp[0],  maintainOffset=True)
     cmds.pointConstraint(spine_spheres[3], spine_spheres[1], spines_grp[2], maintainOffset=True)
     
-    color_node = cmds.shadingNode('lambert', asShader=True, name="Spine_Yellow_Shader")
-    cmds.setAttr(f"{color_node}.color", 1.0, 1.0, 0, type='double3')
-    sg = cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name="Yellow_ShaderSG")
-    cmds.connectAttr(f"{color_node}.outColor", f"{sg}.surfaceShader")
-    cmds.sets(spine_spheres, edit=True, forceElement=sg)
-    cmds.select(clear=True)
-    
-    #legs 
+    #legs
     leftLeg_sphere = cmds.polySphere (name ='Left_Leg_loc' , radius=0.5)[0]
     cmds.setAttr(f"{leftLeg_sphere}.translate", 10, 45, 0, type='double3')
     cmds.setAttr(f"{leftLeg_sphere}.rotateAxis", 0, 180, 90, type='double3')
@@ -71,15 +57,76 @@ def Create_BlueprintSkeleton():
     cmds.setAttr(f"{leftToeEnd_sphere}.translate", 10, 0, 10, type='double3')
     cmds.setAttr(f"{leftToeEnd_sphere}.rotateAxis", 0, -90, 0, type='double3')
 
-    LeftSpheres = [leftLeg_sphere, leftKnee_sphere, leftFoot_sphere, leftToe_sphere, leftToeEnd_sphere]
+    #arm
+    arm_Sphere = []
+    arm_Grp = []
+    spine_spheres.append(sphere)
+    name_arm = ["Left_Clav", "Left_Arm", "Left_Elbow", "Left_Wrist"]
+    for i in range (0,4):
+        name_arm_sphere = name_arm [i]
+        x_pos = 10 + (i) * 10
+        arm_sphere = cmds.polySphere(name=name_arm_sphere, radius=0.5)[0]
+        cmds.setAttr(f"{arm_sphere}.translate", x_pos, 90, 0, type='double3')
+        cmds.setAttr(f"{arm_sphere}.rotate", -90, 0, 0, type='double3')
+        grp_arm =cmds.group(empty=True, name = name_arm_sphere + "_Grp")
+        cmds.matchTransform(grp_arm, arm_sphere)
+        cmds.parent(arm_sphere, grp_arm)
+        arm_Grp.append(grp_arm)
+        arm_Sphere.append(arm_sphere)
+
+    cmds.parentConstraint (spine_spheres[4], arm_Grp[0], maintainOffset=True)
+    cmds.select(clear=True)
+
+    #head
+    spine_spheres.append(sphere)
+    for i in range(1,3):
+        name_neck_sphere = f"Neck_{i:02d}"
+        y_pos = 100 + (i -1) * 5
+        neck_sphere = cmds.polySphere(name=name_neck_sphere, radius=0.5)[0]
+        cmds.setAttr(f"{neck_sphere}.translate", 0, y_pos, 0, type='double3')
+        cmds.setAttr(f"{neck_sphere}.rotate", -90, 0, 90, type='double3')
+        grp_neck = cmds.group(empty=True, name = name_neck_sphere + "_Grp")
+        cmds.matchTransform(grp_neck, neck_sphere)
+        cmds.parent(neck_sphere, grp_neck)
+        cmds.parent(grp_neck, spine_spheres[4])
+    
+    cmds.select(clear=True) 
+
+    # color
+    arm_Grp.append(grp_arm)
+    LeftSpheres = [leftLeg_sphere, leftKnee_sphere, leftFoot_sphere, leftToe_sphere, leftToeEnd_sphere,]
     Color = cmds.shadingNode('lambert', asShader=True, name="Color_Shader")
     cmds.setAttr(f"{Color}.color", 1.0 , 0, 0, type='double3')
-    sg = cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name="Blue_ShaderSG")
+    sg = cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name="Red_Shader")
     cmds.connectAttr(f"{Color}.outColor", f"{sg}.surfaceShader")
     cmds.sets(LeftSpheres, edit=True, forceElement=sg)
+    cmds.sets(arm_Grp, edit=True, forceElement=sg)
+    cmds.select(clear=True)
+
+    color_node = cmds.shadingNode('lambert', asShader=True, name="Color_Shader")
+    cmds.setAttr(f"{color_node}.color", 1.0, 1.0, 0, type='double3')
+    sg = cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name="Yellow_Shader")
+    cmds.connectAttr(f"{color_node}.outColor", f"{sg}.surfaceShader")
+    cmds.sets(spine_spheres, edit=True, forceElement=sg)
+    cmds.select(clear=True)
+
+    Color = cmds.shadingNode('lambert', asShader=True, name="Color_Shader")
+    cmds.setAttr(f"{Color}.color", 1.0 , 1.0, 0, type='double3')
+    sg = cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name="_Shader")
+    cmds.connectAttr(f"{Color}.outColor", f"{sg}.surfaceShader")
+    cmds.sets(pelvis_sphere, edit=True, forceElement=sg)
+    cmds.select(clear=True)
+
+    # Constraint all under Pelvis
+    # Arm
+    arm_Grp.append(grp_arm)
+    arm_Sphere.append(arm_sphere)
+    cmds.parent(arm_Grp[3], arm_Sphere[0])
+    cmds.parent(arm_Grp[2], arm_Sphere[0])
+    cmds.parent(arm_Grp[1], arm_Sphere[0])
+    cmds.pointConstraint(arm_Sphere[1], arm_Sphere[3], arm_Grp[2],  maintainOffset=True)
     cmds.select(clear=True)
     
-    # Constraint all under Pelvis
     # Legs
     cmds.pointConstraint(leftLeg_sphere, leftFoot_sphere, grp,  maintainOffset=True)
     cmds.parent(leftToeEnd_sphere, leftToe_sphere)
