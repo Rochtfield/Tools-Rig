2d67047e42f889eee3e5c73cdc57965a307b510f
Rochtfield <160755865+Rochtfield@users.noreply.github.com>
2026-02-18 19:32:12 +0100

    Add-Head-And-Fix

--- BP_Skeleton.py
+++ BP_Skeleton.py
@@ -74,9 +74,6 @@ def Create_BlueprintSkeleton():
         arm_Grp.append(grp_arm)
         arm_Sphere.append(arm_sphere)
 
-    cmds.parent (arm_Grp[0], spine_spheres[4])
-    cmds.select(clear=True)
-
     #Fingers
     Thumb_Sphere = []
     Index_Sphere = []
@@ -150,23 +147,38 @@ def Create_BlueprintSkeleton():
         Middle_Grp.append(grp_middle)
         Ring_Grp.append(grp_ring)
         Pinky_Grp.append(grp_pinky)
-        
-    cmds.select(clear=True)
 
-    #head
+    #neck
+    Neck_Sphere = []
+    Neck_Grp = []
     spine_spheres.append(sphere)
     for i in range(1,3):
         name_neck_sphere = f"Neck_{i:02d}"
-        y_pos = 100 + (i -1) * 5
+        y_pos = 95 + (i -1) * 5
         neck_sphere = cmds.polySphere(name=name_neck_sphere, radius=0.5)[0]
         cmds.setAttr(f"{neck_sphere}.translate", 0, y_pos, 0, type='double3')
         cmds.setAttr(f"{neck_sphere}.rotate", -90, 0, 90, type='double3')
         grp_neck = cmds.group(empty=True, name = name_neck_sphere + "_Grp")
         cmds.matchTransform(grp_neck, neck_sphere)
         cmds.parent(neck_sphere, grp_neck)
-        cmds.parent(grp_neck, spine_spheres[4])
-    
-    cmds.select(clear=True) 
+        Neck_Sphere.append(neck_sphere)
+        Neck_Grp.append(grp_neck)
+
+    #head
+    Head_Sphere = []
+    Head_Grp = []
+    name_head = ["Head", "HeadEnd"]
+    for i in range(0,2):
+        name_head_sphere = name_head [i]
+        Y_pos = 105+ i * 2
+        head_sphere = cmds.polySphere(name=name_head_sphere, radius=0.5)[0]
+        cmds.setAttr(f"{head_sphere}.translate", 0, Y_pos, 0, type='double3')
+        cmds.setAttr(f"{head_sphere}.rotate", -90, 0, 90, type='double3')
+        grp_head = cmds.group(empty=True, name = name_head_sphere + "_Grp")
+        cmds.matchTransform (grp_head, head_sphere)
+        cmds.parent (head_sphere, grp_head)
+        Head_Sphere.append(head_sphere)
+        Head_Grp.append(grp_head)
 
     # color
     arm_Grp.append(grp_arm)
@@ -195,6 +207,7 @@ def Create_BlueprintSkeleton():
 
     # Constraint all under Pelvis
     # Arm
+    cmds.parent (arm_Grp[0], spine_spheres[4])
     cmds.parent(arm_Grp[3], arm_Sphere[0])
     cmds.parent(arm_Grp[2], arm_Sphere[0])
     cmds.parent(arm_Grp[1], arm_Sphere[0])
@@ -226,17 +239,22 @@ def Create_BlueprintSkeleton():
     cmds.parent(Pinky_Grp[3], Pinky_Sphere[2])
     cmds.parent(Pinky_Grp[2], Pinky_Sphere[1])
     cmds.parent(Pinky_Grp[1], Pinky_Sphere[0])
-    cmds.parent(Pinky_Grp[0], arm_Sphere[3])
+    cmds.parent(Pinky_Grp[0], arm_Sphere[3]) 
 
-    cmds.select(clear=True) 
-
-    
     # Legs
     cmds.pointConstraint(leftLeg_sphere, leftFoot_sphere, grp,  maintainOffset=True)
     cmds.parent(leftToeEnd_sphere, leftToe_sphere)
     cmds.parent(leftToe_sphere, leftFoot_sphere)
     cmds.parent(leftLeg_sphere, pelvis_sphere)
     cmds.parent(grp,pelvis_sphere)
+    
+    #Head - Neck
+    cmds.parent(Neck_Grp[1], Neck_Sphere[0])
+    cmds.parent(Neck_Grp[0], spine_spheres[4])
+    
+    cmds.parent(Head_Grp[1], Head_Sphere[0])
+    cmds.parent(Head_Grp[0], Neck_Sphere[1])
+    
     #Pelvis
     cmds.parent(pelvis_sphere, grpPelvis)
     cmds.parent(leftFoot_sphere, grpPelvis)
